05a26217347677339904a360cf3c6434ef29acc7
aghababyan5 <artyom.a2.a@mail.ru>
2023-07-21 00:00:14 +0400

    some edits

--- User/Assets/JS/header.js
+++ User/Assets/JS/header.js
@@ -3,16 +3,18 @@ document.addEventListener("DOMContentLoaded", () => {
    const reg_ul = document.querySelector(".reg_ul");
    const user_icon = document.querySelector(".user_icon");
    const user_icon_fa = document.querySelector(".fa-user");
-   let userIconClickCount = 0;
+   let userIconIsClicked = false;
 
    user_icon.addEventListener("click", () => {
-      if (userIconClickCount == 0 || userIconClickCount % 2 == 0) {
+      if (userIconIsClicked == false) {
+         userIconIsClicked = true;
          reg_ul.style.display = "block";
          user_icon_fa.style.color = "#fff";
       } else {
+         userIconIsClicked = false;
          reg_ul.style.display = "none";
          user_icon_fa.style.color = "#A09C9C";
       }
-      userIconClickCount++;
+      userIconIsClicked++;
    });
 })
